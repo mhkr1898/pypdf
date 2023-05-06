b56958581d7d46db22366ad79fea1b6f6ae04bd0
mhkr1898 <mike.kristensen@live.dk>
2023-05-06 17:42:09 +0000

    added set_img fun, cleaned pdf example

--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@ const doc = new pdfkit();
 import './settings.js';
 const filePath = '../public/index.pdf';
 doc.pipe(fs.createWriteStream(filePath));
-doc.set_header('status')
-doc.moveTo(0, 20)
-   .lineTo(1, 160)
-   .quadraticCurveTo(1, 200, 150, 120)
-   .bezierCurveTo(1, -40, 200, 200, 300, 150)
-   .lineTo(1, 90)
-   .stroke(12);
-
-const imagePath = '../public/assets/img/tree.jpg';
-const image = doc.openImage(imagePath);
-doc.image(image, {
-   fit: [250, 300],  // set image size
-   align: 'center',  // center the image horizontally
- });
- 
+doc.info['Title'] = 'Hello world!';
+doc.info['Author'] = 'Your Name';
+doc.info['Subject'] = 'Example PDF document';
+doc.info['Keywords'] = 'pdfkit, example, test';
+doc.info['CreationDate'] = new Date();
+doc.info['ModDate'] = new Date();
+doc.set_title('Hello world!')
+doc.set_header('Lorem Ipsum')
+const lorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+console.log(doc.heightOfString(lorem))
+doc.set_body(lorem)
+doc.moveDown(4)
+doc.fontSize(25).text('Centered Image Example', {align: 'center'});
+const bird = '../public/assets/img/bird.jpg';
+doc.set_img(bird, 0.2);
 doc.end();
--- src/settings.js
+++ src/settings.js
@@ -1,9 +1,10 @@
 import pdfkit from 'pdfkit'; 
 
-pdfkit.prototype.set_title = function() {
+pdfkit.prototype.set_title = function(title) {
   this.font('Times-BoldItalic')
-      .fontSize(13)
-      .text('Praksisreflektioner', { align: 'center' });
+      .fontSize(22)
+      .text(title, { align: 'center' });
+  this.moveDown()
 };
 
 pdfkit.prototype.set_header = function(header) {
@@ -17,3 +18,18 @@ pdfkit.prototype.set_body = function(body) {
       .fontSize(10)
       .text(body, { align: 'left' });
 };
+
+pdfkit.prototype.set_img = function(imagePath, imageScale = 0.75) {
+  const pageWidth = this.page.width - this.page.margins.left - this.page.margins.right;
+  // Open image to get properties
+  const image = this.openImage(imagePath);
+  // Calculate the available horizontal space on the page
+  // Scale the image to ARG (default 75%) of its original size
+  const imageWidth = image.width * imageScale; 
+  const availableWidth = pageWidth - imageWidth;
+  // Calculate the horizontal position of the image
+  const horizontalPosition = this.page.margins.left + availableWidth / 2;
+  // Add the image to the PDF document
+  this.image(imagePath, horizontalPosition, this.y, {width: imageWidth});
+}; 
+
