c6dd99ae413f18fd5aa8a0575aebb7a3b4b99d23
mhkr1898 <mike.kristensen@live.dk>
2023-05-14 10:18:50 +0000

    added debounce to filewatcher

--- src/watch.js
+++ src/watch.js
@@ -1,25 +1,36 @@
 import chokidar from 'chokidar';
 import child_process from 'child_process';
 const { exec } = child_process;
-
 const filesToWatch = ['./**/*.js'];
 const watcher = chokidar.watch(filesToWatch, { ignored: /(^|[\/\\])\../ });
 
-watcher.on('change', (path) => {
-  console.log(`Changes detected in ${path}, generating PDF...`);
-  try {
-    const child = exec('node index.js', (err, stdout, stderr) => {
-      if (err) {
-        console.error(`Error: ${err}`);
-      } else {
-        console.log('PDF generation complete');
-      }
-    });
+let timer = null;
+const delay = 500;
 
-    child.stdout.pipe(process.stdout);
-    child.stderr.pipe(process.stderr);
-
-  } catch (err) {
-    console.error(`Error: ${err}`);
+function debounce(callback) {
+  if (timer) {
+    clearTimeout(timer);
   }
+  timer = setTimeout(callback, delay);
+}
+
+watcher.on('change', (path) => {
+  debounce(() => {
+    console.log(`Changes detected in ${path}, generating PDF...`);
+    try {
+      const child = exec('node index.js', (err, stdout, stderr) => {
+        if (err) {
+          console.error(`Error: ${err}`);
+        } else {
+          console.log('PDF generation complete');
+        }
+      });
+
+      child.stdout.pipe(process.stdout);
+      child.stderr.pipe(process.stderr);
+
+    } catch (err) {
+      console.error(`Error: ${err}`);
+    }
+  });
 });
